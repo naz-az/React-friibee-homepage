d939a61551c221969b006590999c33f348714af8
naz-az <87741419+naz-az@users.noreply.github.com>
2021-07-23 06:35:28 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,48 @@
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import 'react-native-gesture-handler';
+
+// import Function from './bottom-tab-bar/Function';
+
+
+// export default function App() {
+//     return (
+//       <>
+      
+//         <Function />
+//         <StatusBar style="auto" />
+//       </>
+//     );
+//   }
+
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import AppNavigator from './SRCC/navigations/Navigator';
+import AppLoading from 'expo-app-loading';
+
+import Function from './bottom-tab-bar/Function';
+
+export default class App extends React.Component{
+  state={
+    isFontLoaded:false
+  }
+  async componentDidMount(){
+    await Font.loadAsync({
+      'Bold':require('./SRCC/fonts/KonnectBold.otf'),
+      'Medium':require('./SRCC/fonts/KonnectMedium.otf'),
+      'Regular':require('./SRCC/fonts/KonnectRegular.otf'),
+    });
+    this.setState({isFontLoaded:true})
+  }
+  render(){
+    return (
+    (this.state.isFontLoaded === true)?(<AppNavigator/>) : (<AppLoading/>)
+      );
+  }
+  
+}
+
+
+  
